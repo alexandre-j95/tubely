1dd8b8f12a22910264140874fd9a45f2f46639c5
Alexandre Jose Afonso Jesus <alexjesus1995@gmail.com>
2026-09-25 15:23:17 +0100

    CH1 L7: Images for thumbnails are now served from and stored in the assets folder

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
 	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,7 +34,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	const maxMemory = 10 << 20
@@ -46,14 +47,27 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	defer file.Close()
 	
 	contentType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
-	imgData, err := io.ReadAll(file)
+	fileExtension:= strings.Split(contentType, "/")
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to read file", err)
+		respondWithError(w, http.StatusBadRequest, "Unable to parse file extension", err)
 		return
 	}
 
-	base64ImgData := base64.StdEncoding.EncodeToString(imgData)
-	dataURL := fmt.Sprintf("data:%s;base64,%s", contentType, base64ImgData)
+	assetPath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoID.String(), fileExtension[1]))
+	destFile, err := os.Create(assetPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file", err)
+		return
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file", err)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, fileExtension[1])
 
 	videoDb, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -65,7 +79,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	videoDb.ThumbnailURL = &dataURL
+	videoDb.ThumbnailURL = &thumbnailURL
 
 	err = cfg.db.UpdateVideo(videoDb)
 	if err != nil {
